0b244484089fcac7d866576114e0eb9e12e996b4
lengbingkeji <122067220+lengbingkeji@users.noreply.github.com>
2023-06-24 22:43:38 +0800

    修改 Main.java
    新增打印内容

--- lengbingkeji-java-javase/src/main/java/com/lengbingkeji/java/javase/Main.java
+++ lengbingkeji-java-javase/src/main/java/com/lengbingkeji/java/javase/Main.java
@@ -12,5 +12,7 @@ package com.lengbingkeji.java.javase;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //新增的功能
+        System.out.println("冷冰科技......");
     }
 }
