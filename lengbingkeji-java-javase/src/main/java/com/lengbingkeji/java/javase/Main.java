9f848dbc393e6de1ad9ac45a682250bedb6309c3
lengbingkeji <1517699467@qq.com>
2023-06-24 22:30:17 +0800

    添加javase模块

---
+++ lengbingkeji-java-javase/src/main/java/com/lengbingkeji/java/javase/Main.java
@@ -0,0 +1,16 @@
+package com.lengbingkeji.java.javase;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.lengbingkeji.java.javase
+ * Description:
+ *
+ * @Author lengbing
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
